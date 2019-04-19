8998c87182e32ded29207e905bb4603cc1966d79
RobinMao225 <robin.mao.gs@gmail.com>
2019-04-19 12:08:14 +0800

    init

---
+++ cal/include/cal.h
@@ -0,0 +1,6 @@
+#ifndef __ORCHID_CAL_H__
+#define __ORCHID_CAL_H__
+
+int add(int a, int b);
+
+#endif
---
+++ temp/src/temp.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"cal.h"
+
+int main()
+{
+    printf("Hello world, 3 + 2 = %d\n", add(3, 2));
+    return 0;
+}
